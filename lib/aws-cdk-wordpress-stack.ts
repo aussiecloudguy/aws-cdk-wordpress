a3495d8b6040b3be1e40475a80d8dc8465343cb7
spudda32 <52263078+spudda32@users.noreply.github.com>
2021-09-08 20:57:04 +1000

    fixed up efs mount code issue

--- lib/aws-cdk-wordpress-stack.ts
+++ lib/aws-cdk-wordpress-stack.ts
@@ -1,7 +1,15 @@
+/*import { Construct, Duration, StackProps, Stack } from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as rds from '@aws-cdk/aws-rds';
+import * as 
+
+
+*/
+
+//CDK2.0
 import { Construct } from "constructs"; 
-import { Duration, aws_iam as iam, aws_rds as rds, aws_secretsmanager as secretsmanager, aws_certificatemanager as certificatemanager, aws_route53 as route53, aws_elasticloadbalancingv2 as elbv2, aws_elasticloadbalancingv2_targets as targets, aws_ec2 as ec2, aws_ecs as ecs, aws_efs as efs, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
-import { FargateService } from "aws-cdk-lib/lib/aws-ecs";
-import { FargateCluster } from "aws-cdk-lib/lib/aws-eks";
+import { Duration, aws_iam as iam, aws_rds as rds, aws_secretsmanager as secretsmanager, aws_certificatemanager as certificatemanager, aws_route53_targets as route53_targets, aws_route53 as route53, aws_elasticloadbalancingv2 as elbv2, aws_elasticloadbalancingv2_targets as targets, aws_ec2 as ec2, aws_ecs as ecs, aws_efs as efs, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 
 
 export interface CdkWordPressStackProps extends StackProps {
@@ -11,6 +19,7 @@ export interface CdkWordPressStackProps extends StackProps {
   readonly auroramaxUnit?: string; //RDS Instance type
   readonly route53domain?: string; //Route 53 Hosted Domain (Optional). If specified ACM will use for Certificate and create HTTPS listener for Load Balancer
   readonly wordpressFQDN?: string;
+  readonly SetRoute53RootURL?: boolean; //Automatically create Alias Record for root DNS and CNAME for WWW on zone
 
 }
 
@@ -24,17 +33,24 @@ export class AwsCdkWordpressStack extends Stack {
     - Load Balancer
     - Security Group
     */
+   let vpcCidr = props?.VpcCidr
+    if (props?.VpcCidr === undefined) {
+  
+      vpcCidr='192.168.0.0/24'
+      
+    }
 
     // Create VPC (/24)
     const vpc=new ec2.Vpc(this,"wordpressVpc",{
-    cidr: '192.168.0.0/24',
+    cidr: vpcCidr,
     maxAzs: 2,
    
     subnetConfiguration: [
       {
         name: 'Application',
         cidrMask: 26,
-        subnetType: ec2.SubnetType.ISOLATED,
+        subnetType: ec2.SubnetType.PRIVATE, //enabled Private access due to ECR requirement
+//        subnetType: ec2.SubnetType.ISOLATED,
         
         
         },
@@ -57,6 +73,7 @@ export class AwsCdkWordpressStack extends Stack {
     
     });
 
+    
     //#########################################################################
     //# Section: VPC Endpoints                                                #
     //#
@@ -68,6 +85,12 @@ export class AwsCdkWordpressStack extends Stack {
       onePerAz: true,
     });
 
+    /*
+    VPC Endpoints for ECR not required at the moment. NAT Gateway used as public Registry cannot resolve to privatelink
+
+    */
+
+    /*
     // Adding interface endpoints for ECR Api
     const ecrIE = vpc.addInterfaceEndpoint('ECR', {
       service: ec2.InterfaceVpcEndpointAwsService.ECR,
@@ -83,6 +106,14 @@ export class AwsCdkWordpressStack extends Stack {
       subnets: appsubnets,
     });
     ecrdkrIE.connections.allowFrom(ec2.Peer.ipv4(vpc.vpcCidrBlock), ec2.Port.tcp(443), 'Allow from ECR IE Private SG');
+*/
+    // Adding interface endpoints for AWS Secrets Manager
+    const secretsIE = vpc.addInterfaceEndpoint('SecretsManager', {
+      service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+      privateDnsEnabled: true,
+      subnets: appsubnets,
+    });
+    secretsIE.connections.allowFrom(ec2.Peer.ipv4(vpc.vpcCidrBlock), ec2.Port.tcp(443), 'Allow from Secrets IE Private SG');
     
 
     vpc.addGatewayEndpoint('S3-GWEndpoint', {
@@ -128,7 +159,7 @@ export class AwsCdkWordpressStack extends Stack {
     
     
         targetGroupHttp.configureHealthCheck({
-          path: "/api/status",
+          path: "/",
           protocol: elbv2.Protocol.HTTP,
         });
         
@@ -146,6 +177,8 @@ export class AwsCdkWordpressStack extends Stack {
         {
           domainName: domainname,
           validation: certificatemanager.CertificateValidation.fromDns(zone),
+          subjectAlternativeNames: [`www.${domainname}`]
+          
         });
 
         
@@ -164,6 +197,22 @@ export class AwsCdkWordpressStack extends Stack {
         targetProtocol: elbv2.ApplicationProtocol.HTTPS,
         targetPort: 443,
       });
+
+      //Create Route 53 Alias record for Route 53 Zone root and CNAME for WWW
+      if (props.SetRoute53RootURL === true) {
+        new route53.ARecord(this, 'AliasRecord', {
+          zone,
+          target: route53.RecordTarget.fromAlias(new route53_targets.LoadBalancerTarget(lb)),
+          
+        });
+
+        new route53.CnameRecord(this, 'WWWCNAME', {
+          recordName: 'www',
+          zone,
+          domainName: zone.zoneName
+        });
+
+      }
     } else {
 
       //HTTP Only listener when no certificate is provided
@@ -188,7 +237,7 @@ export class AwsCdkWordpressStack extends Stack {
 
    // Iterate the data subnets and place into an array
 
-   const selection = vpc.selectSubnets({
+   const datasubnets = vpc.selectSubnets({
      subnetGroupName:  "Data",
      onePerAz: true,
    });
@@ -228,7 +277,7 @@ export class AwsCdkWordpressStack extends Stack {
       lifecyclePolicy: efs.LifecyclePolicy.AFTER_14_DAYS, // files are not transitioned to infrequent access (IA) storage by default
       performanceMode: efs.PerformanceMode.GENERAL_PURPOSE, // default
       removalPolicy: RemovalPolicy.SNAPSHOT,
-      vpcSubnets:selection,
+      vpcSubnets:datasubnets,
       securityGroup:efssg
     });
 
@@ -237,12 +286,12 @@ export class AwsCdkWordpressStack extends Stack {
 
      // Iterate the data subnets and place into an array
      let appsubnetids: Array<string>= [];
-     const appselection = vpc.selectSubnets({
+     const appdatasubnets = vpc.selectSubnets({
        subnetGroupName:  "Application",
        onePerAz: true,
      });
 
-    for (const appsubnet of appselection.subnets) {
+    for (const appsubnet of appdatasubnets.subnets) {
        appsubnetids.push(appsubnet.subnetId).toString;
     }
 
@@ -261,8 +310,7 @@ export class AwsCdkWordpressStack extends Stack {
       generateStringKey: 'password'
     }
 
-  }
-  );
+  });
   
 
     const auroraDatabaseCluster = new rds.ServerlessCluster(this, 'Database', {
@@ -270,16 +318,25 @@ export class AwsCdkWordpressStack extends Stack {
       credentials: rds.Credentials.fromSecret(dbpassword),
       parameterGroup:  rds.ParameterGroup.fromParameterGroupName(this, 'ParameterGroup', 'default.aurora-mysql5.7'),
       defaultDatabaseName: 'wordpress',
+      
       vpc: vpc,
       securityGroups: [dbSecurityGroup],
       //storageEncryptionKey: databaseKey,
-      deletionProtection: false
+      deletionProtection: false,
+      subnetGroup: new rds.SubnetGroup(this, 'auroradbsubnets', {
+        vpcSubnets: datasubnets,
+        vpc: vpc,
+        description: 'subnets for Aurora serverless cluster'
+      })
     });
 
     /*Create Fargate Service, Task and components
     */
  
     // Create the IAM role assumed by the task and its containers
+         
+
+          
           const taskRole = new iam.Role(this, "wordpresstask-role", {
             assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
             roleName: "task-role",
@@ -312,6 +369,7 @@ export class AwsCdkWordpressStack extends Stack {
                     "ecr:GetAuthorizationToken",
                     "ecr:BatchCheckLayerAvailability",
                     "ecr:GetDownloadUrlForLayer",
+                    "ecr-public:GetAuthorizationToken",
                     "ecr:BatchGetImage",
                     "logs:CreateLogStream",
                     "logs:PutLogEvents"
@@ -352,41 +410,43 @@ export class AwsCdkWordpressStack extends Stack {
     
     });
 
-    
-    fargatetask.addContainer('wordpress', {
-      image: ecs.ContainerImage.fromRegistry("bitnami/wordpress"),
+ 
+    const containerdef = new ecs.ContainerDefinition(this, 'wordpress-container', {
+      taskDefinition: fargatetask,
+      image: ecs.ContainerImage.fromRegistry("public.ecr.aws/bitnami/wordpress:latest"),
       memoryLimitMiB:512,
       portMappings: [{
         containerPort: 8080,
         protocol: ecs.Protocol.TCP
       }],
       
+      
       logging:  ecs.LogDrivers.awsLogs ({ streamPrefix: 'WordPressLogs' }),
       environment: {
         
           //Environment variables pass from Aurora Deployment
           MARIADB_HOST: auroraDatabaseCluster.clusterEndpoint.hostname,
-          WORDPRESS_DATABASE_USER: "wordpressadmin",
+          
           WORDPRESS_DATABASE_NAME: 'wordpress',
           PHP_MEMORY_LIMIT: "512M",
           enabled: "false",
-          ALLOW_EMPTY_PASSWORD:"yes"
+          ALLOW_EMPTY_PASSWORD:"no",
+          WORDPRESS_TABLE_PREFIX: 'wp_'
       },
       
-
+      
       secrets: {
-        WORDPRESS_DATABASE_PASSWORD: ecs.Secret.fromSecretsManager(dbpassword, 'password')
+        WORDPRESS_DATABASE_PASSWORD: ecs.Secret.fromSecretsManager(dbpassword, 'password'),
+        WORDPRESS_DATABASE_USER: ecs.Secret.fromSecretsManager(dbpassword, 'username'),
 
       },
-  
-      
-
-
-    
-      
-
     });
 
+      containerdef.addMountPoints({
+      containerPath: '/var/www/html',
+      sourceVolume: 'wordpress-data',
+      readOnly: false,
+    });
     
 
     const ecscluster=new ecs.Cluster(this, "ecscluster", {
@@ -401,15 +461,27 @@ export class AwsCdkWordpressStack extends Stack {
     const fargateService=new ecs.FargateService(this, "fargateservice",{
     taskDefinition: fargatetask,
     cluster: ecscluster,
-    vpcSubnets: appselection,
+    vpcSubnets: appdatasubnets,
     desiredCount:1,
     securityGroups:[fargatesg],
-    serviceName: 'wordpress'
+    serviceName: 'wordpress',
+    
 
     });
 
     fargateService.attachToApplicationTargetGroup(targetGroupHttp);
 
+    //Support Scaling based on performance
+
+    const scaling = fargateService.autoScaleTaskCount({ maxCapacity: 3 });
+scaling.scaleOnCpuUtilization('CpuScaling', {
+  targetUtilizationPercent: 50
+});
+
+scaling.scaleOnRequestCount('RequestScaling', {
+  requestsPerTarget: 10000,
+  targetGroup: targetGroupHttp
+})
  
 }
 
